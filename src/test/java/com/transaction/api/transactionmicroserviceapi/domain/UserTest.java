dbcb61db176183e0f8efd2529b5d9cb0ab0c93a5
Adrian <adrian.gabriel.dev@gmail.com>
2024-01-15 23:39:38 -0300

    commit: adicionando 11 testes em JSON.

---
+++ src/test/java/com/transaction/api/transactionmicroserviceapi/domain/UserTest.java
@@ -0,0 +1,16 @@
+package com.transaction.api.transactionmicroserviceapi.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void deposit() {
+    }
+
+    @Test
+    void withdraw() {
+    }
+}
